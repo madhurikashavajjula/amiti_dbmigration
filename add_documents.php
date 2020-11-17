95cab60d1a13d9429f0968713c0f355865eadc5b
madhuri <madhuri.kashavajjula@gmail.com>
2020-11-17 12:35:16 +0530

    removed unwanted code

--- add_documents.php
+++ add_documents.php
@@ -2,27 +2,10 @@
 	include_once 'config/database.php';
 	include_once "model/db.php";
 	include_once "check_reply.php";
-		
-		
-	
-		
-		
 	
 ?>
 <!DOCTYPE html>
 
-<!-- 
-Template Name: Metronic - Responsive Admin Dashboard Template build with Twitter Bootstrap 4 & Angular 7
-Author: KeenThemes
-Website: http://www.keenthemes.com/
-Contact: support@keenthemes.com
-Follow: www.twitter.com/keenthemes
-Dribbble: www.dribbble.com/keenthemes
-Like: www.facebook.com/keenthemes
-Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
-Renew Support: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
-License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
--->
 <html lang="en">
 
 	<!-- begin::Head -->
@@ -31,7 +14,6 @@ License: You must have a valid license purchased only from themeforest(the above
 		<title>Amiti | Documents</title>
 		<meta name="description" content="Headers datatables examples">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
 		<!--begin::Fonts -->
 		<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
 		<script>
